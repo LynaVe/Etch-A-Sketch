d18473836bce0b7de5bee9e69eba6a5d07666894
LynaVe <96400100+LynaVe@users.noreply.github.com>
2023-04-09 20:37:09 +0800

    create a etch a sketch project for Odin

---
+++ etch.js
@@ -0,0 +1,71 @@
+let createMultipleSquare = document.querySelector('.main-container');
+let squares = createMultipleSquare.querySelectorAll('div');
+let subDiv = document.getElementsByClassName('sub-container');
+let getColor = document.querySelector(".color-picker");
+
+let clickMouse = true;
+
+function createSixteenxSixteen(size){     //create a 16x16 grid box
+    squares.forEach((div) => div.remove());
+    createMultipleSquare.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    createMultipleSquare.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    let amount = size*size;  //change the size of the container
+    for( let i= 0; i < amount ; i++){
+    let multipleDivs = document.createElement('div');
+    multipleDivs.classList.add('sub-container');
+    createMultipleSquare.insertAdjacentElement("beforeend", multipleDivs);
+    }
+}
+
+createSixteenxSixteen(16);
+
+function changeSize(input){
+    createSixteenxSixteen(input);
+}
+
+function multipleColorChange(){ //change the bg color in multiple color
+    
+        for (let i = 0; i < subDiv.length; i++) {
+            subDiv[i].onmouseover = function(e) {
+            let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+            if(clickMouse){
+            e.target.style.backgroundColor = randomColor;
+        }
+    }
+    }
+}
+
+function eraseButton(){ //erase the color
+    
+        for (let i = 0; i < subDiv.length; i++) {
+            subDiv[i].onmouseover = function(e) {
+            if(clickMouse){
+            e.target.style.backgroundColor = null;
+        }
+    }
+    }
+}
+
+function changeSingleColor(){
+    
+    for (let i = 0; i < subDiv.length; i++) {
+        subDiv[i].onmouseover = function(e) {
+        if(clickMouse){
+        e.target.style.backgroundColor = getColor.value;
+        }
+    }
+    }
+}
+
+function clearButton(){
+    let createMultipleSquare = document.querySelector('.main-container');
+    let squares = createMultipleSquare.querySelectorAll('div');
+        for( let i = 0 ; i < squares.length; i++){
+            squares[i].style.backgroundColor = null;
+        }
+}
+
+document.querySelector("body").addEventListener("click",() => {
+    clickMouse = !clickMouse;
+});
